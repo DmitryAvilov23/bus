705c4bf6d9146242b0a58a0e81f27b395af02336
davilov <davilov@embedika.ru>
2026-01-29 15:45:18 +0300

    Added prettier

--- bus/src/app/app/app.component.ts
+++ bus/src/app/app/app.component.ts
@@ -1,16 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    RouterOutlet
-  ],
-  standalone: true
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [RouterOutlet],
+    standalone: true
 })
 export class AppComponent {
-  title = 'Bus tickets';
+    title = 'Bus tickets';
 }
